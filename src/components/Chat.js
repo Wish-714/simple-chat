7a83c8afc686379b35b2e9a0c23d8ab505951ccf
wish-714 <wisnu4562@gmail.com>
2022-05-14 13:29:45 +0700

    commit

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -48,7 +48,7 @@ function Chat () {
                 onChange={(e) => setFormValue(e.target.value)}
                 className="block w-full px-5 py-2.5 mx-2 mb-2 bg-gray-300 rounded-full outline-none focus:text-gray-700"
                 name="message" required
-                placeholder='type here...'/>
+                placeholder='type something here...'/>
                 <button
                 onClick={sendMessage}
                 class="font-bold text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 rounded-full text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
